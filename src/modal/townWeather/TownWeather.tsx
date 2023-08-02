4787c17bbd12969ba8a941bb6fa628b712f4b201
KatsuboDmytro <dmitriyofficial03@gmaik.com>
2023-08-02 14:33:03 +0300

    right side was designed

--- src/modal/townWeather/TownWeather.tsx
+++ src/modal/townWeather/TownWeather.tsx
@@ -1,11 +1,27 @@
 import { FC } from 'react';
+import './townWeather.css';
 
 interface TownWeatherProps {}
 
 export const TownWeather: FC<TownWeatherProps> = () => {
   return (
-    <div className=''>
-      <h1>VIETNAM</h1>
+    <div className='town'>
+      <div className="backgound">
+        <img src="/assets/clouds.svg" alt="clouds" />
+        <img src="/assets/clouds.svg" alt="clouds" />
+        <img src="/assets/clouds.svg" alt="clouds" />
+        <img src="/assets/clouds.svg" alt="clouds" />
+      </div>
+      <div className="townWeather">
+        <div className="dayInTheTown">Sunday</div>
+        <div className="tempertatureInfo">
+          <img src="/assets/cloudy.png" alt="cloudy" />
+          <div>
+            <span className='tempature'>24</span><span className="value">°С</span>
+          </div>
+        </div>
+        <div className="theTown">Kyiv</div>
+      </div>
     </div>
   );
 }
